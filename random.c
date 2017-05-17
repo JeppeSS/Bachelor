6b600f685206059c16dc41d3d10144cc93f1364a
jeppeSS <jeppeskov92@gmail.com>
2017-05-17 11:49:54 +0000

    Added more random generator

--- random.c
+++ random.c
@@ -32,3 +32,42 @@ unsigned long genSeed(void){
 
 }
 
+void randomUniform(mpz_t rand, mpz_t size){
+    
+    gmp_randstate_t randState;
+
+    unsigned long seed = genSeed();
+
+    gmp_randinit_mt(randState);
+    gmp_randseed_ui(randState, seed);
+
+    mpz_urandomm(rand, randState, size);
+}
+
+
+void randomRange(mpz_t rand, int rho){
+
+    mpz_t posNeg;
+
+    mpz_init(posNeg);
+    
+    gmp_randstate_t randState;
+
+    unsigned long seed = genSeed();
+
+    gmp_randinit_mt(randState);
+    gmp_randseed_ui(randState, seed);
+
+    mpz_urandomb(posNeg, randState, 1);
+
+
+    if(mpz_cmp_ui(posNeg, 0)){
+        mpz_urandomb(rand, randState, rho+1);
+        mpz_neg(rand, rand);
+    } else {
+        mpz_urandomb(rand, randState, rho+1);
+
+    }
+    
+
+}
--- random.h
+++ random.h
@@ -1,7 +1,11 @@
 #ifndef RANDOM
 #define RANDOM
 
+#include <gmp.h>
+
 unsigned long genSeed(void);
+void randomUniform(mpz_t rand, mpz_t size);
+void randomRange(mpz_t rand, int rho);
 
 
 #endif
